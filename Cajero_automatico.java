4f2678b10920eb4ee8484bd0ddb97e7fb0b7386b
johnfsala <johnsmoriones@gmail.com>
2025-03-22 13:17:22 -0500

    clases 3

--- Cajero_automatico.java
+++ Cajero_automatico.java
@@ -3,11 +3,18 @@ public class Cajero_automatico {
     public static void main(String[] args) {
         
         int saldo = 5000;
-        int monto= 1000;
+        int monto= 500;
         int total= 0;
         
-        if (salado == 5000 && monto )
-        System.out.println("el resultado de la resta es" );
+        if (saldo> 5000){
+        System.out.println("fondos insuficientes " );}
+        else if (monto> 1000){
+            System.out.println("fondos limite de retiro por transaccion " );
+        }
+       total= saldo - monto; {
+        System.out.println("retiro exitoso, su saldo es: " +total);
+       }
+       
 
     }
 }    
